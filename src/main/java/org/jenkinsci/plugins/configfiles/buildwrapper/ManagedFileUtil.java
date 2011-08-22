7004255aba22591020607768299e2cb6da8a67ff
imod <domi@fortysix.ch>
2011-08-22 18:07:07 +0200

    refactor environment

---
+++ src/main/java/org/jenkinsci/plugins/configfiles/buildwrapper/ManagedFileUtil.java
@@ -0,0 +1,95 @@
+package org.jenkinsci.plugins.configfiles.buildwrapper;
+
+import hudson.FilePath;
+import hudson.remoting.Callable;
+import hudson.remoting.VirtualChannel;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.jenkinsci.lib.configprovider.ConfigProvider;
+import org.jenkinsci.lib.configprovider.model.Config;
+
+public class ManagedFileUtil {
+
+	/**
+	 * creates a tmp file on the given channel
+	 */
+	public static FilePath createTempFile(VirtualChannel channel) throws IOException, InterruptedException {
+		return channel.call(new Callable<FilePath, IOException>() {
+			public FilePath call() throws IOException {
+				final File tmpTarget = File.createTempFile("config", "tmp");
+				return new FilePath(tmpTarget);
+			}
+
+			private static final long serialVersionUID = 1L;
+		});
+	}
+
+	/**
+	 * provisions (publishes) the given files to the workspace.
+	 * 
+	 * @param managedFiles
+	 *            the files to be provisioned
+	 * @param workSpace
+	 *            target workspace
+	 * @param logger
+	 *            the logger
+	 * @return a map of all the files copied, mapped to the path of the remote
+	 *         location
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public static Map<ManagedFile, FilePath> provisionConfigFiles(List<ManagedFile> managedFiles, FilePath workSpace, final PrintStream logger)
+			throws IOException, InterruptedException {
+
+		final Map<ManagedFile, FilePath> file2Path = new HashMap<ManagedFile, FilePath>();
+		logger.print("provisoning config files...");
+
+		for (ManagedFile managedFile : managedFiles) {
+			ConfigProvider provider = getProviderForConfigId(managedFile.fileId);
+			Config configFile = provider.getConfigById(managedFile.fileId);
+
+			boolean isTargetGiven = !StringUtils.isBlank(managedFile.targetLocation);
+
+			FilePath target = null;
+			if (isTargetGiven) {
+				String targetLocation = managedFile.targetLocation;
+				if (!targetLocation.contains(".")) {
+					if (StringUtils.isBlank(targetLocation)) {
+						targetLocation = configFile.name.replace(" ", "_");
+					} else {
+						targetLocation = targetLocation + "/" + configFile.name.replace(" ", "_");
+					}
+				}
+				target = new FilePath(workSpace, targetLocation);
+			} else {
+				target = ManagedFileUtil.createTempFile(workSpace.getChannel());
+			}
+
+			logger.println(Messages.console_output(configFile.name, target.toURI()));
+			ByteArrayInputStream bs = new ByteArrayInputStream(configFile.content.getBytes());
+			target.copyFrom(bs);
+			file2Path.put(managedFile, target);
+		}
+
+		return file2Path;
+	}
+
+	private static ConfigProvider getProviderForConfigId(String id) {
+		if (!StringUtils.isBlank(id)) {
+			for (ConfigProvider provider : ConfigProvider.all()) {
+				if (provider.isResponsibleFor(id)) {
+					return provider;
+				}
+			}
+		}
+		return null;
+	}
+}
