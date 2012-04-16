72ea5db23dd2b8d0d80931ec8c160ce1dfb5a336
imod <domi@fortysix.ch>
2012-04-16 09:30:34 +0200

    make sure action gets removed again

--- src/main/java/org/jenkinsci/plugins/configfiles/buildwrapper/CleanTempFilesRunListener.java
+++ src/main/java/org/jenkinsci/plugins/configfiles/buildwrapper/CleanTempFilesRunListener.java
@@ -9,7 +9,6 @@ import hudson.model.TaskListener;
 import hudson.model.AbstractBuild;
 import hudson.model.listeners.RunListener;
 
-
 /**
  * Removes the temporarily created files at 'onComplete()' of each build, doing it at this state, ensures the files are available also for publishers.
  * 
@@ -21,19 +20,21 @@ public class CleanTempFilesRunListener extends RunListener<AbstractBuild<?, ?>> 
     @Override
     public void onCompleted(AbstractBuild<?, ?> build, TaskListener listener) {
         CleanTempFilesAction action = build.getAction(CleanTempFilesAction.class);
-
         if (action != null) {
-            for (FilePath fp : action.tempFiles) {
-                try {
-                    if (fp != null && fp.exists()) {
-                        fp.delete();
+            try {
+                for (FilePath fp : action.tempFiles) {
+                    try {
+                        if (fp != null && fp.exists()) {
+                            fp.delete();
+                        }
+                    } catch (Exception e) {
+                        listener.getLogger().println("[WARN] failed to delete temp file: " + fp.getRemote());
                     }
-                } catch (Exception e) {
-                    listener.getLogger().println("[WARN] failed to delete temp file: " + fp.getRemote());
                 }
+            } finally {
+                // remove the action, there is nothing we want to persist on the build
+                build.getActions().remove(action);
             }
-            // remove the action, there is nothing we want to persist on the build
-            build.getActions().remove(action);
         }
     }
 
