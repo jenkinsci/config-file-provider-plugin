8b8a1b0836d6a087d6da9ed55108e163fd92a66e
René Scheibe <rene.scheibe@gmail.com>
2019-09-02 22:50:40 +0200

    [JENKINS-53399] Add deserialization test for FolderConfigFileProperty
    The config files for the new test have been set up as similar as
    possible to the existing one, only ids differ. The existing test
    was cleaned a bit.

--- src/test/java/org/jenkinsci/plugins/configfiles/folder/FolderConfigFilesTest.java
+++ src/test/java/org/jenkinsci/plugins/configfiles/folder/FolderConfigFilesTest.java
@@ -1,20 +1,22 @@
-package org.jenkinsci.plugins.configfiles;
+package org.jenkinsci.plugins.configfiles.folder;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 
 import java.util.Collection;
 
+import com.cloudbees.hudson.plugins.folder.Folder;
 import org.jenkinsci.lib.configprovider.model.Config;
+import org.jenkinsci.plugins.configfiles.ConfigFileStore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.recipes.LocalData;
 
 /**
- * Test for {@link GlobalConfigFiles} to ensure reading data.
+ * Test for {@link FolderConfigFileProperty} to ensure reading data.
  */
-public class GlobalConfigFilesTest {
+public class FolderConfigFilesTest {
 
     @Rule
     public JenkinsRule j = new JenkinsRule();
@@ -28,7 +30,7 @@ public class GlobalConfigFilesTest {
     @LocalData
     @Test
     public void verifyLoadWithAnonymousInnerClassComparatorVar1() {
-        ConfigFileStore store = j.getInstance().getExtensionList(GlobalConfigFiles.class).get(GlobalConfigFiles.class);
+        ConfigFileStore store = ((Folder) j.jenkins.getItemByFullName("test-folder")).getAction(FolderConfigFileAction.class).getStore();
         Collection<Config> configs = store.getConfigs();
         assertThat(configs, hasSize(2));
     }
