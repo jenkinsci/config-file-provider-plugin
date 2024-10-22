88956aa5d6ad0bb26eb9fd5e758c725c4a8b86e8
Shlomo Dahan <64103471+shlomomdahan@users.noreply.github.com>
2024-10-22 15:58:56 -0400

    [JENKINS-73891] Extract inline JS in `ConfigFilesUI/edit.jelly` and `ConfigFilesUI/show.jelly` (#342)

---
+++ src/main/resources/org/jenkinsci/plugins/configfiles/ConfigFilesUI/setup-codemirror.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const textarea = document.getElementById("config.content");
+    const contentTypeElement = document.querySelector(".content-type-config");
+
+    const contentType = contentTypeElement.dataset.contentType;
+    const readOnly = contentTypeElement.dataset.readOnly === "true";
+
+    CodeMirror.fromTextArea(textarea, {
+        lineNumbers: true,
+        matchBrackets: true,
+        mode: contentType,
+        readOnly: readOnly,
+    });
+});
