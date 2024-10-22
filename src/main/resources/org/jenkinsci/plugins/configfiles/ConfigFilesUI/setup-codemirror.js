329fa5e38d51dc1c46ca229e05e113901fdf16c3
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-10-22 12:36:03 -0400

    address comments

--- src/main/resources/org/jenkinsci/plugins/configfiles/ConfigFilesUI/setup-codemirror.js
+++ src/main/resources/org/jenkinsci/plugins/configfiles/ConfigFilesUI/setup-codemirror.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const textarea = document.querySelector("textarea#config\\.content");
-    const contentTypeElement = document.querySelector('span[data-content-type]');
+    const contentTypeElement = document.querySelector("span[data-content-type]");
 
-    const contentTypeVal = contentTypeElement.getAttribute('data-content-type');
-    const readOnly = contentTypeElement.hasAttribute('data-read-only');
+    const contentTypeVal = contentTypeElement.getAttribute("data-content-type");
+    const readOnly = contentTypeElement.hasAttribute("data-read-only");
 
     CodeMirror.fromTextArea(textarea, {
         lineNumbers: true,
         matchBrackets: true,
         mode: contentTypeVal,
-        readOnly: readOnly
+        readOnly: readOnly,
     });
 });
